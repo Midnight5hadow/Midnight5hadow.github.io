21a961852337f112e58cfcb2c2330b71efdc6f47
Midnight5hadow <114875103+Midnight5hadow@users.noreply.github.com>
2022-10-08 15:09:03 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -36,69 +36,60 @@ function playpause() {
 
 
 window.onload = function() {
-  
-    var file = document.getElementById("thefile");
     var audio = document.getElementById("audio");
-    audio.crossOrigin = "anonymous";
+    audio.crossOrigin = "anonymous"; // This may or may not do something
     
-    file.onchange = function() {
-        // var files = this.files;
+    var context = new AudioContext();
+    var src = context.createMediaElementSource(audio);
+    var analyser = context.createAnalyser();
 
-        // audio.src = URL.createObjectURL(files[0]);
-        // audio.load();
-        
-        var context = new AudioContext();
-        var src = context.createMediaElementSource(audio);
-        var analyser = context.createAnalyser();
-
-        var canvas = document.getElementById("canvas");
-        
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
-        
-        var ctx = canvas.getContext("2d");
-
-        src.connect(analyser);
-        analyser.connect(context.destination);
-
-        analyser.fftSize = 256;
-
-        var bufferLength = analyser.frequencyBinCount;
-
-        var dataArray = new Uint8Array(bufferLength);
-
-        var WIDTH = canvas.width;
-        var HEIGHT = canvas.height;
-
-        var barWidth = (WIDTH / bufferLength) * 2.5;
-        var barHeight;
-        var x = 0;
+    var canvas = document.getElementById("canvas");
     
-        function renderFrame() {
-            requestAnimationFrame(renderFrame);
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    
+    var ctx = canvas.getContext("2d");
 
-            x = 0;
+    src.connect(analyser);
+    analyser.connect(context.destination);
 
-            analyser.getByteFrequencyData(dataArray);
+    analyser.fftSize = 256;
 
-            ctx.fillStyle = "rgba(255, 255, 255, 0.5)";
-            ctx.fillRect(0, 0, WIDTH, HEIGHT);
+    var bufferLength = analyser.frequencyBinCount;
 
-            for (var i = 0; i < bufferLength; i++) {
-                barHeight = dataArray[i];
-                
-                var r = barHeight + (25 * (i/bufferLength));
-                var g = 250 * (i/bufferLength);
-                var b = 50;
-        
-                ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ", 0.5)";
-                ctx.fillRect(x, HEIGHT - barHeight, barWidth + 1.1, barHeight);
-        
-                x += barWidth + 1;
-            }
+    var dataArray = new Uint8Array(bufferLength);
+
+    var WIDTH = canvas.width;
+    var HEIGHT = canvas.height;
+
+    var barWidth = (WIDTH / bufferLength) * 2.5;
+    var barHeight;
+    var x = 0;
+
+    function renderFrame() {
+        requestAnimationFrame(renderFrame);
+
+        x = 0;
+
+        analyser.getByteFrequencyData(dataArray);
+
+        ctx.fillStyle = "rgba(255, 255, 255, 0.5)";
+        ctx.fillRect(0, 0, WIDTH, HEIGHT);
+
+        for (var i = 0; i < bufferLength; i++) {
+            barHeight = dataArray[i];
+            
+            var r = barHeight + (25 * (i/bufferLength));
+            var g = 250 * (i/bufferLength);
+            var b = 50;
+    
+            ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ", 0.5)";
+            ctx.fillRect(x, HEIGHT - barHeight, barWidth + 1.1, barHeight);
+    
+            x += barWidth + 1;
         }
-    
-        audio.play();
-        renderFrame();
-    };
+    }
+
+    audio.play();
+    renderFrame();
 };
